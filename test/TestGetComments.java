8d03f88c1c5d6225b9995b689bedae4bb2183b57
kexua <kexuansong@gmail.com>
2020-11-09 10:55:25 -0500

    test update

---
+++ test/TestGetComments.java
@@ -0,0 +1,88 @@
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.CommentThread;
+import com.google.api.services.youtube.model.CommentThreadListResponse;
+import models.Comments;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+
+public class TestGetComments {
+    @InjectMocks
+    private String VideoId;
+
+    @Spy
+    private List<CommentThread> results = new ArrayList<>();
+
+    @Test
+    public void TestGetComments() throws IOException {
+        Comments comments = mock(Comments.class);
+        MockitoAnnotations.initMocks(this);
+
+
+        YouTube youtube = mock(YouTube.class, RETURNS_DEEP_STUBS);
+        Assert.assertTrue(youtube instanceof YouTube);
+
+        YouTube.CommentThreads c = mock(YouTube.CommentThreads.class, RETURNS_DEEP_STUBS);
+        Assert.assertTrue(c instanceof YouTube.CommentThreads);
+
+        YouTube.CommentThreads.List l = mock(YouTube.CommentThreads.List.class, RETURNS_DEEP_STUBS);
+        Assert.assertTrue(l instanceof YouTube.CommentThreads.List);
+
+        CommentThreadListResponse r = mock(CommentThreadListResponse.class, RETURNS_DEEP_STUBS);
+        Assert.assertTrue(r instanceof CommentThreadListResponse);
+
+
+
+        when(youtube.commentThreads().list(any())).thenReturn(l);
+
+        youtube.commentThreads().list(any());
+        assertEquals(l,youtube.commentThreads().list(any()));
+
+
+        when(l.setKey(any()).setVideoId("1").setMaxResults(any())).thenReturn(l);
+        l.setKey(any()).setVideoId("1").setMaxResults(any());
+        assertEquals(l,l.setKey(any()).setVideoId("1").setMaxResults(any()));
+
+        List<String> commentlist = new ArrayList<>();
+
+        CommentThread t1 = mock(CommentThread.class, RETURNS_DEEP_STUBS);
+        CommentThread t2 = mock(CommentThread.class, RETURNS_DEEP_STUBS);
+        CommentThread t3 = mock(CommentThread.class, RETURNS_DEEP_STUBS);
+        results.add(t1);
+        results.add(t2);
+        results.add(t3);
+        when(r.getItems()).thenReturn(results);
+        r.getItems();
+        verify(r).getItems();
+        assertEquals(results, r.getItems());
+
+
+        for(CommentThread t : results){
+            String comment = t.getSnippet().getTopLevelComment().getSnippet().getTextDisplay();
+            commentlist.add(comment);
+        }
+
+        when(t3.getSnippet().getTopLevelComment().getSnippet().getTextDisplay()).thenReturn("t3");
+        assertEquals(3, commentlist.size());
+
+
+
+        when(comments.getComments("1")).thenReturn(commentlist);
+
+        assertEquals(commentlist, comments.getComments("1"));
+
+
+    }
+
+
+}
--- test/TestSearchComment.java
+++ test/TestSearchComment.java
@@ -1,76 +1,43 @@
-import com.google.inject.Inject;
-import models.*;
-import com.google.api.services.youtube.YouTube;
-import static org.junit.Assert.*;
+import models.Comments;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
 
-
-import org.mockito.*;
-import java.io.IOException;
-import java.security.GeneralSecurityException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-
-import static org.mockito.Mockito.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.spy;
 
 public class TestSearchComment {
     @InjectMocks
     private String VideoId;
 
-//    @Before
-//    public void setUp(){
-//        Comments c = spy(new Comments(VideoId));
-//        MockitoAnnotations.initMocks(this);
-//    }
+
+
+    @Before
+    public void setUp(){
+        MockitoAnnotations.initMocks(this);
+    }
 
 
     @Test
-    public void SearchCommentTest1(){
-        Comments comments = spy(new Comments(VideoId));
+    public void SearchCommentTest(){
+        Comments c = new Comments(VideoId);
+        Comments spyC = spy(c);
+
         MockitoAnnotations.initMocks(this);
-        List<String> a = Arrays.asList("we have \uD83D\uDE04","all of the");
+
+        List<String> a = Arrays.asList("we have \uD83E\uDD21","all of the \uD83D\uDC7B");;
 
 
-        String resultA = comments.SearchComment(a);
+        assertEquals("\uD83D\uDE10", spyC.SearchComment(a));
+        assertEquals("CommentDisabled", spyC.SearchComment(null));
 
-
-        verify(comments).SearchComment(a);
-        assertEquals("\uD83D\uDC4D", resultA);
 
 
     }
-
-//    @Test
-//    public void SearchCommentTest2(){
-//        Comments comments2 = spy(new Comments(VideoId));
-//        MockitoAnnotations.initMocks(this);
-//
-//        List<String> b = Arrays.asList("we have \uD83D\uDE41","all of the");
-//
-//        String resultB = comments2.SearchComment(b);
-//
-//        verify(comments2).SearchComment(b);
-//        assertEquals("\uD83D\uDC4E", resultB);
-//
-//
-//    }
-
-//    @Test
-//    public void SearchCommentTest3(){
-//        Comments comments3 = spy(new Comments(VideoId));
-//        MockitoAnnotations.initMocks(this);
-//
-//        List<String> c = Arrays.asList("we have \uD83E\uDD0F","all of the");
-//
-//        String resultC = comments3.SearchComment(c);
-//
-//        verify(comments3).SearchComment(c);
-//        assertEquals("\uD83D\uDE10", resultC);
-//
-//    }
 
 
 
