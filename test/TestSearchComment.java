e3808f3d929def4bdfee06678ef90f2e575d37f0
kexua <kexuansong@gmail.com>
2020-11-10 19:15:36 -0500

    test update

--- test/TestSearchComment.java
+++ test/TestSearchComment.java
@@ -1,8 +1,11 @@
 import models.Comments;
 import org.junit.Before;
-import org.junit.Test;
+
+//import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
+import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
@@ -11,34 +14,31 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.spy;
 
 public class TestSearchComment {
-    @InjectMocks
-    private String VideoId;
-
-
-
-    @Before
-    public void setUp(){
-        MockitoAnnotations.initMocks(this);
-    }
 
 
     @Test
-    public void SearchCommentTest(){
-        Comments c = new Comments(VideoId);
-        Comments spyC = spy(c);
-
-        MockitoAnnotations.initMocks(this);
-
-        List<String> a = Arrays.asList("we have \uD83E\uDD21","all of the \uD83D\uDC7B");;
+    public void SearchCommentTest1(){
+        Comments comments = new Comments("1");
+        Comments comments2 = new Comments("1");
 
 
-        assertEquals("\uD83D\uDE10", spyC.SearchComment(a));
-        assertEquals("CommentDisabled", spyC.SearchComment(null));
+        List<String> a = Arrays.asList("we have \uD83D\uDE05","all of the\uD83D\uDE06");
+        List<String> b = Arrays.asList("we have \u2639\uFE0F","all of the \uD83D\uDE41","and \uD83D\uDE04");
+        List<String> c = Arrays.asList("we have \uD83E\uDDB5","all of the \uD83D\uDC40");
 
+        String result1 = comments.SearchComment(a);
+//        String result2 = comments2.SearchComment(b);
+//        String result3 = comments.SearchComment(c);
+
+
+        assertAll(
+                () -> assertEquals("\uD83D\uDC4D", result1),
+//                () -> assertEquals("\uD83D\uDC4E", result2),
+//                () -> assertEquals("\uD83D\uDE10", result3),
+                () -> assertEquals("CommentDisabled", comments.SearchComment(null))
+        );
 
 
     }
-
-
 
     }
